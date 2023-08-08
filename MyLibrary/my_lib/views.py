f8eb8d0bf3fe5c5d3be1d50bd770ef228a6c2012
P.Seferyniak <p.seferyniak@gmail.com>
2023-08-08 22:18:31 +0200

    Models

--- MyLibrary/my_lib/views.py
+++ MyLibrary/my_lib/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic import View
+from .models import Books
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'my_lib/home.html')
+class Home(View):
+    def get_books(self):
+        books = Books.objects.all().order_by('id')
+        for book in books:
+            if book.read == True:
+                book.read_display = 'Tak'
+            else:
+                book.read_display = 'Nie'
+        return books
+
+    def get(self, request):
+        books = self.get_books()
+
+        context = {
+            'books': books
+        }
+        return render(request, 'my_lib/home.html', context)
